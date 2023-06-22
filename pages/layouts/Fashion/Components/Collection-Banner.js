5db7477c55213efe801e8711065855247777a0d4
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2023-06-23 03:45:58 +0600

    make sub banner in single row

--- pages/layouts/Fashion/Components/Collection-Banner.js
+++ pages/layouts/Fashion/Components/Collection-Banner.js
@@ -9,7 +9,7 @@ const Data = [
   {
     img: banner1,
     about: "fragrances",
-    about2:"",
+    about2: "",
     offer: "10% off",
     link: "/left-sidebar/collection",
     class: "p-right text-center",
@@ -17,36 +17,52 @@ const Data = [
   {
     img: banner2,
     about: "cosmetics",
-    about2:"",
+    about2: "",
     offer: "10% off",
     link: "/left-sidebar/collection",
     class: "p-right text-center",
   },
-
+  {
+    img: banner3,
+    about: "candles & ",
+    about2: "diffusers",
+    offer: "10% off",
+    link: "/left-sidebar/collection",
+    class: "p-right text-center",
+  },
 ];
 const Data2 = [
   {
     img: banner3,
     about: "candles & ",
-    about2:"diffusers",
+    about2: "diffusers",
     offer: "10% off",
     link: "/left-sidebar/collection",
     class: "p-right text-center",
   },
 ];
-const MasterCollectionBanner = ({ img, about,about2, offer, link, classes }) => {
+const MasterCollectionBanner = ({
+  img,
+  about,
+  about2,
+  offer,
+  link,
+  classes,
+}) => {
   return (
-    <Col md="6">
+    <Col md="4">
       <Link href={link}>
         <a>
           <div className={`collection-banner ${classes}`}>
             <Media src={img} className="img-fluid" alt="" />
-            <div className="contain-banner" style={{backgroundColor:"rgba(255,255,255,0.5)"}}>
+            <div
+              className="contain-banner"
+              style={{ backgroundColor: "rgba(255,255,255,0.5)" }}
+            >
               <div>
                 <h4>{offer}</h4>
                 <h2>{about}</h2>
-                {about2!="" && <h2>{about2}</h2>}
-
+                {about2 != "" && <h2>{about2}</h2>}
               </div>
             </div>
           </div>
@@ -62,23 +78,24 @@ const CollectionBanner = () => {
       {/*collection banner*/}
       <section className="pb-0">
         <div>
-        <Container>
-          <Row className="partition2">
-            {Data.map((data, i) => {
-              return (
-                <MasterCollectionBanner
-                  key={i}
-                  img={data.img.src}
-                  about={data.about}
-                  link={data.link}
-                  offer={data.offer}
-                  classes={data.class}
-                />
-              );
-            })}
-          </Row>
-        </Container>
-        <Container >
+          <Container>
+            <Row className="partition2">
+              {Data.map((data, i) => {
+                return (
+                  <MasterCollectionBanner
+                    key={i}
+                    img={data.img.src}
+                    about={data.about}
+                    about2={data.about2}
+                    link={data.link}
+                    offer={data.offer}
+                    classes={data.class}
+                  />
+                );
+              })}
+            </Row>
+          </Container>
+          {/* <Container >
           <Row className="partition2" style={{justifyContent: "center",
 alignItems: "center", paddingTop:30}} >
             {Data2.map((data, i) => {
@@ -95,7 +112,7 @@ alignItems: "center", paddingTop:30}} >
               );
             })}
           </Row>
-        </Container>
+        </Container> */}
         </div>
       </section>
       {/*collection banner end*/}
